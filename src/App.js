f38e79498c60722a32d5395d63e8012f4370b969
Soumanpaul <souman1729@gmail.com>
2019-10-22 01:32:39 +0530

    Starting projects

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Header } from "./components/layout/Herder";
+
+export const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
---
+++ src/components/layout/Herder.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Header = () => {
+  
+  const x = 1;
+  
+  
+  return <header className="header" data-testid="header">
+    <nav>
+      <div className="logo">
+        <img src="/images/logo.png" alt="Todolist" />
+      </div>
+      
+    </nav>
+  </header>
+};
